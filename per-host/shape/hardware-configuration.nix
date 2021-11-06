9acad40015f9b67d238b429852833ba58c18812d
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2021-11-05 23:48:19 -0700

    Use the latest kernel instead of the default LTS one.

--- per-host/shape/hardware-configuration.nix
+++ per-host/shape/hardware-configuration.nix
@@ -11,5 +11,4 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
 }
