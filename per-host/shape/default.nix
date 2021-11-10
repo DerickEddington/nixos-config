62fbf981e0be104385e6dc9de5a6faff21ff0e17
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2021-11-09 21:18:20 -0800

    Improve Xserver config for HDMI external monitor.

--- per-host/shape/default.nix
+++ per-host/shape/default.nix
@@ -1,8 +1,12 @@
 # Options specific to this particular host machine.
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-with builtins;
+let
+  inherit (builtins) elem;
+  inherit (lib.lists) optional;
+  inherit (lib.strings) optionalString;
+in
 
 {
   imports = [
@@ -18,6 +22,12 @@ with builtins;
      in
        ext-tcc.module)
   ];
+
+  # TODO?: Maybe some options.my.xserver that fit my laptop's different GPUs and
+  # display outputs and my monitor, which formalize how I want each combination
+  # and which make it easy to switch, and which serve as a record of what I
+  # figure out for them, and which control how the xserver config below is
+  # constructed.
 
   # Define this again here to ensure it is checked that this is the same as what
   # /etc/nixos/configuration.nix also defined for the same option.
@@ -61,6 +71,9 @@ with builtins;
     extraModulePackages = with config.boot.kernelPackages; [ ];
 
     kernelParams = [
+      "video=HDMI-A-1:3440x1440@100"  # Use 100 Hz, like xserver.
+      "video=eDP-1:d"  # Disable internal lid screen.
+
       "tuxedo_keyboard.state=0"              # backlight off
       "tuxedo_keyboard.brightness=25"        # low, if turned on
       "tuxedo_keyboard.color_left=0xff0000"  # red, if turned on
@@ -103,6 +116,31 @@ with builtins;
   # Drivers from Tuxedo Computers that also work on my Clevo NH55EJQ.
   hardware.tuxedo-keyboard.enable = true;  # Also enabled by the next below.
   hardware.tuxedo-control-center.enable = true;
+
+  services.xserver = {
+    exportConfiguration = true;
+
+    xrandrHeads = [
+      {
+        # (The kernel names this same monitor HDMI-A-1 for some reason.)
+        output = "HDMI-A-0";
+        primary = true;
+        # This Modeline is not what the cvt nor gtf utilities give, because what
+        # they give (which are a little different between the two) for 100 Hz
+        # causes my monitor to fallback to 60 Hz for some reason.  This Modeline
+        # is the same that my previous laptop used with this same monitor, and
+        # 100 Hz works and the monitor does not switch modes when switching
+        # users or switching virtual consoles (in conjunction with the video
+        # argument in kernelParams above).
+        monitorConfig = optionalString (elem "video=HDMI-A-1:3440x1440@100" config.boot.kernelParams) ''
+          Modeline "3440x1440@100.0"  543.50  3440 3488 3552 3600  1440 1443 1453 1510 -hsync +vsync
+          Option "PreferredMode" "3440x1440@100.0"
+        '';
+      }
+    ] ++ (optional (!(elem "video=eDP-1:d" config.boot.kernelParams)) {
+      output = "eDP";
+    });
+  };
 
   # TODO: Enable this once my /etc/nixos/configuration.nix stays constant.
   # system.autoUpgrade.enable = true;
