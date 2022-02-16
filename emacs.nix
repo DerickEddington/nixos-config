32e83de8e1af31eb2758bc11d1c7af00902ace59
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2022-02-15 18:48:29 -0800

    Change emacs.nix to use latest MELPA archive from nixos-unstable.
    I might later rearrange which Emacs packages come from the Stable MELPA.  The
    important thing is that now this choice is possible.

--- emacs.nix
+++ emacs.nix
@@ -1,33 +1,53 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
   baseEmacs = pkgs.emacs;
   emacsWithPackages = (pkgs.emacsPackagesFor baseEmacs).emacsWithPackages;
+
+  # Use the latest list of what MELPA has, so that updated versions of Emacs
+  # packages in MELPA are available to us sooner.
+  latestMelpaArchive = let
+    channel = <nixos-unstable>;
+    pkg = "pkgs/applications/editors/emacs";
+    archive = "elisp-packages/recipes-archive-melpa.json";
+  in
+    channel + "/${pkg}/${archive}";
+  melpaOverride =
+    { archiveJson = latestMelpaArchive; };
 in
-  emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
-      ivy
-      counsel
-      ivy-hydra
-      lsp-ivy
-      rg
-      ibuffer-project
-      home-end
-      all-the-icons
-      cargo
-      lsp-mode
-      lsp-ui
-      magit
-      multiple-cursors
-      smartparens
-      flycheck
-      flycheck-rust
-      rust-mode
-      go-mode
-      nasm-mode
-      company
-      toml-mode
-      nix-mode
-    ]) ++ (with epkgs.elpaPackages; [
-      gnu-elpa-keyring-update
-    ])
+emacsWithPackages
+  (epkgs:
+    let
+      melpaPackages       = epkgs.melpaPackages.override       melpaOverride;
+      melpaStablePackages = epkgs.melpaStablePackages.override melpaOverride;
+    in
+      (with melpaStablePackages; [
+        ibuffer-project
+        magit
+      ]) ++
+      (with melpaPackages; [
+        ivy
+        counsel
+        ivy-hydra
+        lsp-ivy
+        rg
+        home-end
+        all-the-icons
+        cargo
+        lsp-mode
+        lsp-ui
+        multiple-cursors
+        smartparens
+        flycheck
+        flycheck-rust
+        rust-mode
+        go-mode
+        nasm-mode
+        company
+        toml-mode
+        nix-mode
+      ]) ++
+      (with epkgs.elpaPackages; [
+        gnu-elpa-keyring-update
+      ])
   )
