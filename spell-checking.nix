ced9635b9ca08a07188292993a707ca3c00a5bd5
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2025-05-26 19:05:35 -0700

    Use aspellWithDicts instead of custom aspell.conf.
    This seems to be the more-proper way that Nixpkgs provides for achieving what I was doing.

--- spell-checking.nix
+++ spell-checking.nix
@@ -3,29 +3,9 @@
 {
   environment = {
     systemPackages = with pkgs; [
-      aspell
-      aspellDicts.en
-      aspellDicts.en-computers
-      aspellDicts.en-science
+      (aspellWithDicts (dicts: with dicts; [en en-computers en-science]))
       hunspellDicts.en_US-large  # Mainly for LibreOffice.
     ];
-
-    # Needed because NixOS no longer automatically exports ASPELL_CONF, and its new patching of
-    # Aspell to use $NIX_PROFILES seems to not work.
-    variables.ASPELL_CONF = "conf-dir /etc";
-
-    etc."aspell.conf".text = ''
-      # This requires that the env var ASPELL_CONF is defined to use this file,
-      # which is done by my custom NixOS module.
-
-      # Use the union of whatever multiple packages provide.
-      data-dir /run/current-system/sw/lib/aspell
-
-      # Requires aspellDicts.en-computers package
-      add-extra-dicts en-computers.rws
-      # Requires aspellDicts.en-science package
-      add-extra-dicts en_US-science.rws
-    '';
   };
 
   my.allowedUnfree = [ "aspell-dict-en-science" ];
